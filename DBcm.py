b821a48f88f512638779b61ea3b39ec9b7d4809f
dima-in <dima-in@mail.ru>
2023-01-29 16:17:25 +0300

    29.01.23 * Добавлена обработка исключений при подключеии к БД.
    * Добавлено выполнение log_request() в отдельном потоке

--- DBcm.py
+++ DBcm.py
@@ -1,19 +1,55 @@
 import mysql.connector
 
 
+class ConnectionError(Exception):
+    """
+    ошибка соеенения с БД
+    """
+    pass
+
+
+class CredentialError(Exception):
+    """
+    ошибка ввода учетных данных БД,
+    приводит к появлению ProgrammingError
+    может возникать в ходе выполнения __enter__
+
+    """
+    pass
+
+
+class SQLError(Exception):
+    """
+    ошибка SQL-запроса
+    """
+    pass
+
+
 class UseDatabase:
 
     def __init__(self, config: dict) -> None:
         self.configuration = config
 
     def __enter__(self) -> 'cursor':
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mysql.connector.errors.InterfaceError as err:
+            raise ConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise ConnectionError(err)
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
-
-
+        """
+        Если возникло ProgrammingError-возбудить SQLError.
+        Возбудить исключение после выполнения 
+        кода метода __exit__ 
+        """
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_val)
+        elif exc_type:
+            raise exc_type(exc_val)
--- hello_flask.py
+++ hello_flask.py
@@ -1,47 +1,31 @@
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialError, SQLError
 from httpagentparser import detect
-from flask import Flask, render_template, request, escape, session
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from checker import check_logged_in
+from threading import Thread
+from time import sleep
 
 app = Flask(__name__)
 
 app.secret_key = 'metallica'
 
+
+@app.route('/login/')
+def login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in'
+
+
+@app.route('/logout/')
+def logout() -> str:
+    session.pop('logged_in')  # ключ удаляется, чтобы избежать KeyError
+    return 'You are now logged out'
+
+
 app.config['dbconfig'] = {'host': '127.0.0.1',
                           'user': 'vsearch',
                           'password': 'vsearchpasswd',
                           'database': 'vsearchlogDB', }
-
-
-
-
-def log_request(req: 'flask_request', res: str) -> None:
-    with UseDatabase(app.config['dbconfig']) as cursor:
-
-        _SQL = """insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL,
-                       (req.form['phrase'],
-                        req.form['letters'],
-                        req.remote_addr,
-                        detect(req.environ.get('HTTP_USER_AGENT'))['browser']['name'],
-                        res,))
-
-
-@app.route('/search4', methods=['POST'])
-def do_search() -> 'html':
-    phrase = request.form['phrase']
-    letters = request.form['letters']
-    title = 'Вот результаты:'
-    submit_data = 'Были введены следующие данные: '
-    results = str(search4letters(phrase, letters))
-    log_request(request, results)
-    return render_template('results.html',
-                           the_title=title,
-                           the_phrase=phrase,
-                           the_letters=letters,
-                           the_results=results,
-                           the_submit_data=submit_data,
-                           )
 
 
 @app.route('/')
@@ -53,31 +37,65 @@ def enrty_page() -> 'html':
                            the_clickbutton='Когда будете готовы, нажмите эту кнопку:')
 
 
-@app.route('/login/')
-def login() -> str:
-    session['logged_in'] = True
-    return 'You are now logged in'
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Вот результаты:'
+    submit_data = 'Были введены следующие данные: '
+    results = str(search4letters(phrase, letters))
 
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
 
-@app.route('/logout/')
-def logout() -> str:
-    session.pop('logged_in') # ключ удаляется, чтобы избежать KeyError
-    return 'You are now logged out'
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            sleep(15)
+            _SQL = """insert into log (phrase, letters, ip, browser_string, results) 
+            values (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL,
+                           (req.form['phrase'],
+                            req.form['letters'],
+                            req.remote_addr,
+                            detect(req.environ.get('HTTP_USER_AGENT'))['browser']['name'],
+                            res,))
+
+    try:
+        log_thread = Thread(target=log_request, args=(request, results))
+        log_thread.start()
+    except ConnectionError as err:
+        print('Ошибка подключения к базе данных', str(err))
+    except SQLError as err:
+        print('Ошибка SQL запроса', str(err))
+    except Exception as err:
+        print('Ошибка в do_serch: ', str(err))
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,
+                           the_submit_data=submit_data,
+                           )
 
 
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
-
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """ select phrase, letters, ip, browser_string, results from log """
-        cursor.execute(_SQL)
-        list_tuple = cursor.fetchall()
-        titles = ('Фраза', 'Буквы', 'IP адрес', 'Браузер', 'Результаты')
-        return render_template('viewlog.html',
-                               the_title='Show log',
-                               the_row_titles=titles,
-                               the_data=list_tuple, )
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """ select phrase, letters, ip, browser_string, results from log """
+            cursor.execute(_SQL)
+            list_tuple = cursor.fetchall()
+            titles = ('Фраза', 'Буквы', 'IP адрес', 'Браузер', 'Результаты')
+            return render_template('viewlog.html',
+                                   the_title='Show log',
+                                   the_row_titles=titles,
+                                   the_data=list_tuple, )
+    except ConnectionError as err:
+        print('База данных не подключена? Ошибка: ', str(err))
+    except CredentialError as err:
+        print('Неверный логин или пароль. Ошибка: ', str(err))
+    except Exception as err:
+        print('Обнаружена ошибка:', str(err))
 
 
 def search4letters(phrase: str, letter: str) -> set:
